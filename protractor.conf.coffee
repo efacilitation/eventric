abb0c2a57b2b33de66ea9cbc14fc573eaef0eeb7
alexlawrence <mail@alex-lawrence.com>
2015-02-24 18:38:09 +0100

    Remove obsolete protractor files

--- protractor.conf.coffee
+++
@@ -1,9 +0,0 @@
-config =
-
-  seleniumAddress: 'http://localhost:4444/wd/hub'
-
-  specs: [
-    'docs/app/src/**/*.coffee'
-  ]
-
-module.exports = config
--- protractor.conf.js
+++
@@ -1,3 +0,0 @@
-// This is required until protractor supports preprocessors
-require('coffee-script/register');
-exports.config = require('./protractor.conf.coffee');
